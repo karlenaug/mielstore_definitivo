bbe55876de110e29ac5d25f99bbaa326780dd73b
karlenaug <augustokarlen66@gmail.com>
2025-05-28 11:12:56 -0300

    animacion.css

--- plantilla/head.php
+++ plantilla/head.php
@@ -7,9 +7,6 @@
 <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 <link rel="stylesheet" href="assets/css/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-<link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-  />
 <link rel="stylesheet" href="assets/css/contacto y nosotros.css">
+<link href="https://cdn.jsdelivr.net/gh/yesiamrocks/cssanimation.io@1.0.3/cssanimation.min.css" rel="stylesheet">
 </head>
