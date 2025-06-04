3b53f5b37d794d781401472ff8bf69326e5c197e
karlenaug <augustokarlen66@gmail.com>
2025-06-03 23:05:37 -0300

    actualizacion de animaciones y mapa en contacto

---
+++ contenido-index.php
@@ -0,0 +1,55 @@
+ <body class="cfondo">
+      <!--Titulo-->
+      <h1 class="titulo centrado cssanimation leFadeIn sequence" data-scrolly-down="leFadeIn">Miel Store</h1>
+      <h2 class="titulo centrado cssanimation leFadeIn sequence" data-scrolly-down="leFadeIn">El sabor de la naturaleza</h2>
+      <!--carrusel-->
+      <div class="carrusel" id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000" data-scrolly-down="leFadeIn">
+        <div class="carousel-inner">
+          <div class="carousel-item active opacidad">
+            <img src="assets/img/bees-4126065_1920.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item opacidad">
+            <img src="assets/img/honey-352205_1920.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item opacidad">
+            <img src="assets/img/mieles frascos.jpg" class="d-block w-100" alt="...">
+          </div>
+            <div class="texto-overlay texto-overlay-p rounded-5 fw-bold cssanimation leFadeIn sequence">
+           <p>¡Bienvenido a Mielstore! Sumérgete en un mundo donde cada gota de miel es un descubrimiento natural. Aquí celebramos el arte de la apicultura, ofreciendo miel artesanal, pura y llena de sabor, que endulzará cada rincón de tus días. De la colmena a tu mesa, te invitamos a vivir una experiencia auténtica y cautivadora que despierta los sentidos. ¡Descubre la esencia de la naturaleza en cada frasco y déjate conquistar por el dulce encanto de Mielstore!</p>
+       </div>
+        </div>
+      </div>
+    </div>
+<!--Cards-->
+   <section class="mt-5 container-xl">
+  <div class="card-group">
+    <div class="card border border-5 card text-bg-warning mb-3" data-scrolly-down="leFadeIn">
+      <img src="assets/img/miel de abeja clara.jpg" class="card-img-top container-foto-card" alt="Miel clara">
+      <div class="card-body">
+        <h5 class="card-title">Miel refinada 250ml</h5>
+        <p class="card-text">Esta miel está limpia, sin ningún agregado ni restos del panal.</p>
+        <h5 class="fw-bold">$4000</h5>
+        <a href="#" class="btn btn-dark">Añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-5 card text-bg-warning mb-3" data-scrolly-down="leFadeIn">
+      <img src="assets/img/miel con panal.jpg" class="card-img-top container-foto-card" alt="Miel con panal">
+      <div class="card-body">
+        <h5 class="card-title">Miel con panal 250ml</h5>
+        <p class="card-text">Esta miel está potenciada por sus agregados directos del panal.</p>
+        <h5 class="fw-bold">$6000</h5>
+        <a href="#" class="btn btn-dark">añadir al carrito</a>
+      </div>
+    </div>
+    <div class="card border border-5 card text-bg-warning mb-3" data-scrolly-down="leFadeIn">
+      <img src="assets/img/miel de panal.jpg" class="card-img-top container-foto-card" alt="panal">
+      <div class="card-body">
+        <h5 class="card-title">Panal comestible 250gr</h5>
+        <p class="card-text">Esta miel, aunque poco común, muy rica en sabor, es una alternativa a la manera tradicional de consumir la miel, en este caso se mastica, aportando unas sensaciones únicas.</p>
+        <h5 class="fw-bold">$5300</h5>
+        <a href="#" class="btn btn-dark">Añadir al carrito</a>
+      </div>
+    </div>
+  </div>
+ 
+</section>
---
+++ contenido-nosotros.php
@@ -0,0 +1,36 @@
+<body class="cfondo">
+  <img class="conteiner-img-nosotros" data-scrolly-down="leFadeIn" src="assets/img/nosotros fabrica.jpg" alt="">    
+    <!-- Sección: Nuestra Historia -->
+  <section id="nuestra-historia" class="container bloques-distancia">
+    <h2 class="fw-bold container-nosotros-titulos rounded-4" data-scrolly-down="leFadeIn">Un viaje dulce y natural</h2>
+    <p data-scrolly-down="leFadeIn">
+      Nuestra historia nace de una pasión genuina por la apicultura y el deseo de compartir el auténtico sabor de la miel artesanal. Desde nuestros comienzos, hemos trabajado con dedicación para capturar la esencia de la naturaleza en cada frasco, manteniendo tradiciones y métodos respetuosos con el medio ambiente.
+    </p>
+  </section>
+   <!-- Sección: Misión y Visión -->
+  <section id="mision-vision">
+    <div class="container bloques-distancia" data-scrolly-down="leFadeIn">
+      <h2 class="fw-bold container-nosotros-titulos rounded-4 "> Misión y Visión</h2>
+      <div class="mv-container bloques-distancia">
+        <div class="mv-item">
+          <h3 class="fw-bold container-nosotros-titulos rounded-4 "> Misión</h3>
+          <p>Endulzar la vida mientras cuidamos del medio ambiente y promovemos una apicultura responsable.</p>
+        </div>
+        <div class="mv-item bloques-distancia">
+          <h3 class="fw-bold container-nosotros-titulos rounded-4 "> Visión</h3>
+          <p>Ser la referencia de miel artesanal en el mercado, uniendo tradición, innovación y sostenibilidad.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+   <!-- Sección: Nuestro Equipo -->
+  <section id="equipo" class="container bloques-distancia" data-scrolly-down="leFadeIn">
+    <h2 class="fw-bold container-nosotros-titulos rounded-4 ">Detrás de cada frasco, un equipo comprometido</h2>
+    <div class="team-container">
+      <div class="team-member">
+        <img src="assets/img/WhatsApp Image 2025-05-13 at 17.35.40.jpeg" class="container-foto bloques-distancia" alt="Miembro del equipo">
+        <h3 class="bloques-distancia fw-bold">Augusto Karlen</h3>
+        <p>Apicultor Principal</p>
+      </div>
+    </div>
+  </section>
