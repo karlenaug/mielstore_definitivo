3e1d391343ed20f7babf9a917593a7d0d0aed392
karlenaug <augustokarlen66@gmail.com>
2025-06-11 09:31:37 -0300

    act

--- register.php
+++ register.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <?php include 'plantilla/head.php'; ?>
 <?php include 'plantilla/navbar.php'; ?>
 <?php include 'contenidos/contenido-register.php'; ?>
-=======
-<?php include 'plantilla/head.php'; ?>
-<?php include 'plantilla/navbar.php'; ?>
-<?php include 'contenidos/contenido-register.php'; ?>
->>>>>>> f94244ddb9c2d565411fdfe86570d783169f42fa
 <?php include 'plantilla/footer.php' ; ?>
