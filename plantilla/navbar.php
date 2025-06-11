f94244ddb9c2d565411fdfe86570d783169f42fa
karlenaug <augustokarlen66@gmail.com>
2025-06-10 21:23:36 -0300

    registro agregado

---
+++ plantilla/navbar.php
@@ -0,0 +1,33 @@
+<header> 
+<nav class="navbar bg-warning" data-bs-theme="light">
+  <div class="container-fluid">
+    <a class="navbar-brand titulo" href="index.php">MielStore</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="nosotros.php">Nostros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacto.php">Contacto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="productos.php">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Registrarse</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </header>
